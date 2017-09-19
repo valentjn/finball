49814351e73a4e9b60b4c3bf1c9d8cffc051a2ef
Sebastian Hasler <sebastian.hasler@gmx.net>
2017-09-19 09:13:30 +0200

    Shader fix: explicit uniform location

--- src/visualization/world_vert.glsl
+++ src/visualization/world_vert.glsl
@@ -1,5 +1,5 @@
 #version 330
-#define GL_ARB_explicit_uniform_location 1
+#extension GL_ARB_explicit_uniform_location : require
 
 layout(location = 0) uniform mat4 model;
 layout(location = 1) uniform mat4 view;

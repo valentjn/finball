942fa7de0d266889a08d098c65fc57d78b17ba81
Sebastian Hasler <sebastian.hasler@gmx.net>
2017-09-19 08:59:29 +0200

    Explicitly specify OpenGL context version

--- src/visualization/CRenderer.cpp
+++ src/visualization/CRenderer.cpp
@@ -70,6 +70,9 @@ CRenderer::CRenderer()
 	if (!m_window)
 		throw std::runtime_error(SDL_GetError());
 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 	SDL_GLContext m_glcontext = SDL_GL_CreateContext(m_window);
 	if (!m_glcontext)
 		throw std::runtime_error(SDL_GetError());

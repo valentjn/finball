af4d151b9241616c9b2b8020f0eeac721b67df5e
Christoph Str <christoph.stroessner@gmail.com>
2017-09-19 09:10:17 +0200

    Initialization of fi

--- src/CStage_FluidSimulationLBM.hpp
+++ src/CStage_FluidSimulationLBM.hpp
@@ -74,64 +74,56 @@ public:
 		if (!input_cDataArray2D.isValidData())
 			return;
 
-		if (	output_Velocity.width != input_cDataArray2D.width		||
-				output_Velocity.height != input_cDataArray2D.height
-		)
+		// initialization in the first iteration
+		if (	output_Field.width != input_cDataArray2D.width || output_Field.height != input_cDataArray2D.height)
 		{
-			output_Velocity.resize(input_cDataArray2D.width, input_cDataArray2D.height);
-			output_Density.resize(input_cDataArray2D.width, input_cDataArray2D.height);
-			fi_Array.resize(input_cDataArray2D.width, input_cDataArray2D.height);
+			initialization();
 		}
 
-// TODO initialize fi in the first iteration
-
-
-// TODO find boundary cells and save their position them in a readable structure
-
-		/*
-		 * process input/output/boundary flags
-		 */
-		for (int y = 0; y < simulationData_Buffer1.height; y++)
-		{
- 			for (int x = 0; x < simulationData_Buffer1.width; x++)
-			{
-				unsigned char flag = input_cDataArray2D.getRef(x,y);
-
-				switch(flag)
-				{
-				case 1:	// boundary
-					simulationData_Buffer1.getRef(x,y,0) = 0;
-					simulationData_Buffer1.getRef(x,y,1) = 0;
-					break;
-
-				case 2:	// inflow
-					simulationData_Buffer1.getRef(x,y,0) = 1;
-					simulationData_Buffer1.getRef(x,y,1) = 0;
-					break;
-
-				case 3:	// outflow
-					simulationData_Buffer1.getRef(x,y,0) = -1;
-					simulationData_Buffer1.getRef(x,y,1) = 0;
-					break;
-
-				default:
-					break;
-				}
-			}
-		}
 
 //TODO Calculate fi collision
 
 //TODO Calculate fi streaming
 
-//TODO Consider Boundary in Col & STream
+//TODO Consider Boundary in Col & STream (1 = boundary, 2 = inflow, 3 = outflow)
 
 //TODO Calculate macroscopic quantities
 
 
 		pipeline_push();
 
-//TODO
+	// Set fi_old = fi_new
+	for (int y = 0; y < input_cDataArray2D.height; y++)
+	{
+		for (int x = 0; x < input_cDataArray2D.width; x++)
+		{
+			for (int i = 0; i < 9; i ++)
+			{
+					fi_Old.getRef(x,y,i) = fi_New.getRef(x,y,i);	
+			}
+		}
+	}
+	}
+
+private:
+	void initialization()
+	{
+		// initialize all the array sizes
+		output_Field.resize(input_cDataArray2D.width, input_cDataArray2D.height);
+		fi_New.resize(input_cDataArray2D.width, input_cDataArray2D.height);
+		fi_Old.resize(input_cDataArray2D.width, input_cDataArray2D.height);
+		// initialize fi in the first iteration
+		for (int y = 0; y < input_cDataArray2D.height; y++)
+		{
+			for (int x = 0; x < input_cDataArray2D.width; x++)
+			{
+				for (int i = 0; i < 9; i ++)
+				{
+					fi_New.getRef(x,y,i) = -1;
+					fi_Old.getRef(x,y,i) = -1;	
+				}
+			}
+		}
 	}
 
 

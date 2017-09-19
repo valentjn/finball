eb20a4e91b1b8c49dba9218e20116e5d77080e85
Boris-Chengbiao Zhou <bobo1239@web.de>
2017-09-20 01:44:55 +0200

    Start integrating bullet

--- src/RigidBody.hpp
+++ src/RigidBody.hpp
@@ -1,6 +1,11 @@
 #ifndef RIGID_BODY_HPP_
 #define RIGID_BODY_HPP_
 
+#include <memory>
+
+#include "btBulletDynamicsCommon.h"
+#include "glm/vec2.hpp"
+
 #include "Parameters.hpp"
 #include "Level.hpp"
 #include "RigidBodyInput.hpp"
@@ -11,16 +16,39 @@ private:
     Parameters &parameters;
     Level &level;
 
+    std::unique_ptr<btAlignedObjectArray<btCollisionShape *>> collision_shapes;
+
+    std::unique_ptr<btCollisionConfiguration> collision_configuration;
+    std::unique_ptr<btCollisionDispatcher> dispatcher;
+    std::unique_ptr<btBroadphaseInterface> broadphase;
+    std::unique_ptr<btConstraintSolver> solver;
+    std::unique_ptr<btDiscreteDynamicsWorld> dynamics_world;
+
 public:
     RigidBody(Parameters &parameters, Level &level) :
         parameters(parameters),
-        level(level)
+        level(level),
+        collision_shapes(std::make_unique<btAlignedObjectArray<btCollisionShape *>>()),
+        collision_configuration(std::make_unique<btDefaultCollisionConfiguration>()),
+        dispatcher(std::make_unique<btCollisionDispatcher>(collision_configuration.get())),
+        broadphase(std::make_unique<btDbvtBroadphase>()),
+        solver(std::make_unique<btSequentialImpulseConstraintSolver>()),
+        dynamics_world(std::make_unique<btDiscreteDynamicsWorld>(
+            dispatcher.get(), broadphase.get(), solver.get(), collision_configuration.get()))
     {}
 
     void compute(const RigidBodyInput &input,
                  RigidBodyOutput &rigidBodyOutput)
     {
-        // TODO
+        auto &grid_obj = rigidBodyOutput.grid_objects;
+        auto &grid_vel = rigidBodyOutput.grid_velocities;
+        for (int j = 0; j < dynamics_world->getNumCollisionObjects(); j++) {
+            auto &obj = dynamics_world->getCollisionObjectArray()[j];
+            // TODO: determine which cells are occupied by obj
+            grid_obj->setValue(1, 3, RigidBodyOutput::type::DYNAMIC);
+            grid_vel->setValue(1, 3, glm::vec2 {1.0, 0.5});
+        }
+
     }
 };
 
--- src/RigidBodyOutput.hpp
+++ src/RigidBodyOutput.hpp
@@ -1,8 +1,27 @@
 #ifndef RIGID_BODY_OUTPUT_HPP_
 #define RIGID_BODY_OUTPUT_HPP_
 
-class RigidBodyOutput {
+#include <memory>
 
+#include <glm/vec2.hpp>
+
+// TODO: coordinate this with other teams
+const int GRID_Y = 42;
+const int GRID_X = 42;
+
+class RigidBodyOutput {
+public:
+    enum class type {
+        INFLOW, OUTFLOW, DYNAMIC, STATIC
+    };
+
+    const unique_ptr<Array2D<type>> grid_objects;
+    const unique_ptr<Array2D<glm::vec2>> grid_velocities;
+
+    RigidBodyOutput() :
+        grid_objects(std::make_unique<Array2D<type>>(GRID_X, GRID_Y)),
+        grid_velocities(std::make_unique<Array2D<glm::vec2>>(GRID_X, GRID_Y))
+    {}
 };
 
 #endif

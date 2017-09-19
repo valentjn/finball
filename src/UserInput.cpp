36afdbc3c558623f580d6048dec5dacd0bc3d44a
Marcial Gaißert <st141606@stud.uni-stuttgart.de>
2017-09-19 21:58:09 +0200

    Basic UserInput class

---
+++ src/UserInput.cpp
@@ -0,0 +1,21 @@
+#include "UserInput.hpp"
+
+// headers for kinect
+#include <XnCppWrapper.h>
+
+UserInput::UserInput(Parameters &parameters)
+	: parameters(parameters), context(std::make_unique<xn::Context>())
+{
+	//TODO
+}
+
+UserInput::~UserInput()
+{
+	//TODO
+	context->Release();
+}
+
+void UserInput::getInput(UserInputOutput &userInputOutput)
+{
+	//TODO
+}
--- src/UserInput.hpp
+++ src/UserInput.hpp
@@ -4,18 +4,25 @@
 #include "Parameters.hpp"
 #include "UserInputOutput.hpp"
 
+// standard library includes
+#include <memory>
+
+// forward declarations for kinect
+namespace xn
+{
+  class Context;
+}
+
 class UserInput {
 private:
     Parameters &parameters;
+	std::unique_ptr<xn::Context> context;
 
 public:
-    UserInput(Parameters &parameters) : parameters(parameters)
-    {}
+    UserInput(Parameters &parameters);
+	~UserInput();
 
-    void getInput(UserInputOutput &userInputOutput)
-    {
-        // TODO
-    }
+    void getInput(UserInputOutput &userInputOutput);
 };
 
 #endif

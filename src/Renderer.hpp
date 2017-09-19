8f4a77fd48f06bedfd1c8d0fffd173ce28e8e9f4
Benedikt Werner <1benediktwerner@gmail.com>
2017-09-20 00:04:57 +0200

    Renderer opens blank SDL window

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,16 +1,31 @@
 #ifndef RENDERER_HPP_
 #define RENDERER_HPP_
 
+#include <SDL2/SDL.h>
+
 #include "Parameters.hpp"
 #include "RendererInput.hpp"
 
 class Renderer {
 private:
     Parameters &parameters;
+    SDL_Window *window;
 
 public:
     Renderer(Parameters &parameters) : parameters(parameters)
-    {}
+    {
+        SDL_Init(SDL_INIT_VIDEO);
+        window = SDL_CreateWindow("Smiley in SDL",
+                                  SDL_WINDOWPOS_UNDEFINED,
+                                  SDL_WINDOWPOS_UNDEFINED,
+                                  500, 500, SDL_WINDOW_SHOWN);
+    }
+
+    ~Renderer()
+    {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
 
     void render(const RendererInput &rendererInput)
     {

d705cd5fa846d1d4e15d2e01af8f54a912c072bf
Marcial Gaißert <st141606@stud.uni-stuttgart.de>
2017-09-21 11:06:14 +0200

    Fake user input

--- src/UserInput.hpp
+++ src/UserInput.hpp
@@ -2,6 +2,7 @@
 #define USER_INPUT_HPP_
 
 #include <SDL2/SDL.h>
+#include <chrono>
 
 #include "Parameters.hpp"
 #include "UserInputOutput.hpp"
@@ -23,6 +24,11 @@ public:
                 break;
             }
         }
+		auto time_p = std::chrono::high_resolution_clock::now();
+		long t = std::chrono::duration_cast<std::chrono::milliseconds>(time_p.time_since_epoch()).count();
+		long steps = 1000;
+		userInputOutput.anglePlayer1 = (t%steps)*90.0/steps;
+		userInputOutput.anglePlayer2 = (t%steps)*90.0/steps;
     }
 };
 
--- src/UserInputOutput.hpp
+++ src/UserInputOutput.hpp
@@ -3,11 +3,25 @@
 
 #include "SDL2/SDL.h"
 
+class UserInput;
+
 class UserInputOutput {
+	double anglePlayer1, anglePlayer2;
 public:
     bool quit;
 
     UserInputOutput() : quit(false) {}
+
+	double getAngle(int player)
+	{
+		switch(player){
+		case 1: return anglePlayer1;
+		case 2: return anglePlayer2;
+		}
+		return 0;
+	}
+
+	friend class UserInput;
 };
 
 #endif

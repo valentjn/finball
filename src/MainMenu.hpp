5cb1f2e5420c2c95a3aacd7f226c87b069eaf038
Benedikt Seidl <benedikt.seidl@tum.de>
2017-09-21 17:02:42 +0200

    Rename ico to png

--- src/MainMenu.hpp
+++ src/MainMenu.hpp
@@ -34,6 +34,9 @@ public:
         SDL_Color color_light = {255, 255, 255, 0};
         SDL_Color color_dark = {50, 50, 50, 0};
 
+        SDL_Surface *icon_surface = IMG_Load("assets/haicon.png");
+        SDL_SetWindowIcon(window, icon_surface);
+
         SDL_Surface *background_surface = IMG_Load("assets/background.jpg");
         SDL_Texture *background_texture =
             SDL_CreateTextureFromSurface(renderer, background_surface);

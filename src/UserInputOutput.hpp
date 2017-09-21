131bc5758a11e49c347061b9ef5fd62e225326e0
Marcial Gaißert <st141606@stud.uni-stuttgart.de>
2017-09-21 18:16:08 +0200

    Adjust fake data to needed values

--- src/UserInputOutput.hpp
+++ src/UserInputOutput.hpp
@@ -8,16 +8,25 @@ class UserInput;
 class UserInputOutput {
 	double leftAngle[2];
 	double rightAngle[2];
+	double leftVelocity[2];
+	double rightVelocity[2];
+	
 public:
-    bool quit;
+    bool quit, start;
 
-    UserInputOutput() : quit(false) {}
+    UserInputOutput() : quit(false), start(false) {}
 
 	double getLeftAngle(int player) {
 		return leftAngle[player-1];
 	}
 	double getRightAngle(int player) {
 		return rightAngle[player-1];
+	}
+	double getLeftVelocity(int player) {
+		return leftVelocity[player-1];
+	}
+	double getRightVelocity(int player) {
+		return rightVelocity[player-1];
 	}
 
 	friend class UserInput;

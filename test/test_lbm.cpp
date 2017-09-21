876a9e9409df41bf67cffe96372d9249acbfc7cd
Nathan W Brei <nathan.w.brei@gmail.com>
2017-09-21 21:22:45 +0200

    Dummy inputs for test case

--- test/test_lbm.cpp
+++ test/test_lbm.cpp
@@ -2,5 +2,21 @@
 #include "../src/LatticeBoltzmann.hpp"
 
 TEST(LBMTest, CollideStep1x1) {
-  EXPECT_TRUE(true);
+
+  Parameters parameters;
+  Level level;
+  LatticeBoltzmannInput input;
+  LatticeBoltzmannOutput output;
+
+  level.width=1;
+  level.height=1;
+  output.matrix = std::make_unique<Array2D<glm::vec3>>(1,1);
+  output.matrix->value(0,0) = glm::vec3(0,0,0);
+
+  LatticeBoltzmann sut(parameters, level, false);
+  sut.compute(input, output);
+  std::cout << output.matrix->value(0,0)[0] << std::endl;
+  std::cout << output.matrix->value(0,0)[1] << std::endl;
+  std::cout << output.matrix->value(0,0)[2] << std::endl;
+  EXPECT_EQ(1,level.width);
 }

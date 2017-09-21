6ea435fec143963c046f7a1ecee11d9fccb49682
Nathan W Brei <nathan.w.brei@gmail.com>
2017-09-21 14:45:10 +0200

    Skeleton of a CollideStep

--- test/test_lbm.cpp
+++ test/test_lbm.cpp
@@ -1,6 +1,15 @@
 #include "gtest/gtest.h"
 #include "../src/LatticeBoltzmann.hpp"
+#include "../src/LatticeBoltzmannInput.hpp"
+#include "../src/LatticeBoltzmannOutput.hpp"
 
 TEST(LBMTest, CollideStep1x1) {
   EXPECT_TRUE(true);
+  LatticeBoltzmann sut(nullptr, nullptr);
+  LatticeBoltzmannInput input(nullptr, nullptr);
+  LatticeBoltzmannOutput output;
+  sut.compute(input, output);
+
+  
+
 }

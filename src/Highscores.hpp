10cc763bef64d28a556d9775827e5a1d767587cc
Benedikt Werner <1benediktwerner@gmail.com>
2017-09-21 15:39:13 +0200

    Added highscore saving to Highscores class

--- src/Highscores.hpp
+++ src/Highscores.hpp
@@ -1,16 +1,41 @@
 #ifndef HIGHSCORES_HPP_
 #define HIGHSCORES_HPP_
 
+#define HIGHSCORE_CUTOFF 10
+
 #include <algorithm>
 #include <iostream>
+
+using namespace std;
 
 class Highscores {
 private:
     static void sortHighscores(std::vector<float> &highscores) {
-        std::sort(highscores.begin(), highscores.end(), [=](float a, float b) -> bool { return a > b; });
+        sort(highscores.begin(), highscores.end(), [=](float a, float b) -> bool { return a > b; });
+    }
+
+    static void saveHighscores(vector<float> &highscores) {
+        fstream file;
+        file.open("build/haiscores.txt", fstream::out);
+        if (file.is_open()) {
+            for (auto const &highscore) {
+                file << highscore << "\n";
+            }
+        } else {
+            cerr << "Failed to save Haiscores" << endl;
+        }
     }
 
 public:
+    static void saveHighscore(float highscore) {
+        vector<float> highscores;
+        loadHighscores(highscores);
+        highscores.push_back(highscore);
+        sortHighscores(highscores);
+        highscores.resize(HIGHSCORE_CUTOFF);
+        saveHighscores(highscores);
+    }
+
     static void loadHighscores(std::vector<float> &highscores) {
         fstream file;
         file.open("build/haiscores.txt", fstream::in);
@@ -24,6 +49,7 @@ public:
             highscores.push_back(highscore);
         }
         sortHighscores(highscores);
+        highscores.resize(HIGHSCORE_CUTOFF);
     }
 };
 

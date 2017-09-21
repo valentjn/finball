adb6ea7c1bb91642b05caf14d5b6ddb7a36fce79
christianKuchler <christian.kuchler@fau.de>
2017-09-21 23:18:59 +0200

    Update test_lbm

--- test/test_lbm.cpp
+++ test/test_lbm.cpp
@@ -20,3 +20,22 @@ TEST(LBMTest, CollideStep1x1) {
   std::cout << output.matrix->value(0,0)[2] << std::endl;
   EXPECT_EQ(1,level.width);
 }
+
+TEST(LatticBoltzmannTest, EquilibriumTrue) {
+
+  Level level;
+  LatticeBoltzmannInput input;
+  LatticeBoltzmannOutput output;
+
+  level.width=1;
+  level.height=1;
+  output.matrix = std::make_unique<Array2D<glm::vec3>>(1,1);
+  output.matrix->value(0,0) = glm::vec3(0,0,0);
+
+  LatticeBoltzmann sut( level, false);
+  sut.compute(input, output);
+  std::cout << "xVelocity" << output.matrix->value(0,0)[0] << std::endl;
+  std::cout << "yVelocity" << output.matrix->value(0,0)[1] << std::endl;
+  std::cout << "Density " << output.matrix->value(0,0)[2] << std::endl;
+ 
+}

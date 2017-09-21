911cff65b6aaa9b2a2727aaae3b64cd8b5dddb7b
Frank von Bleifrei <freifrauvonbleifrei@gmx.net>
2017-09-21 11:01:16 -0400

    just ideas so far

--- src/RigidBodyPhysics.hpp
+++ src/RigidBodyPhysics.hpp
@@ -43,8 +43,19 @@ public:
             // TODO: determine which cells are occupied by obj
             grid_obj->value(1, 3) = RigidBodyPhysicsOutput::type::DYNAMIC;
             grid_vel->value(1, 3) = glm::vec2{1.0, 0.5};
+            computeForces(obj, input);
         }
     }
+
+    //btRigidBody.applyImpulse (const btVector3 &impulse, const btVector3 &rel_pos)
+    // iterate over objects
+    void computeForces( btCollisionObject*& obj , const RigidBodyPhysicsInput &input, const float& realtimestepinseconds = 0.1) {
+        // iterate over rigid body outer cells
+            // iterate over neighboring fluid cells, take connecting f_i in old and new frame.
+                // sum them and multiply factors: d, (delta_x/delta_t)^2, real_delta_t,
+    }
+
+    //void getImpulseDiff( btCollisionObject*& obj )
 };
 
 #endif

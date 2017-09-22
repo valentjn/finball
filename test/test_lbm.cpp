83d02a8218c5a1fc0bcd698e0c5b6d3dd00ac929
Nathan W Brei <nathan.w.brei@gmail.com>
2017-09-22 21:28:30 +0200

    Updated to use new data structures, again

--- test/test_lbm.cpp
+++ test/test_lbm.cpp
@@ -3,39 +3,38 @@
 
 TEST(LBMTest, CollideStep1x1) {
 
-  Parameters parameters;
-  Level level;
-  LatticeBoltzmannInput input;
-  LatticeBoltzmannOutput output;
-
+  Level level("data/testLevel.txt");
   level.width=1;
   level.height=1;
-  output.matrix = std::make_unique<Array2D<glm::vec3>>(1,1);
-  output.matrix->value(0,0) = glm::vec3(0,0,0);
 
-  LatticeBoltzmann sut(parameters, level, false);
+  LatticeBoltzmannInput input;
+  LatticeBoltzmannOutput output(level);
+  output.density.value(0,0) = 0;
+  output.velocity.value(0,0) = glm::vec2(0,0);
+
+  LatticeBoltzmann sut(level);
   sut.compute(input, output);
-  std::cout << output.matrix->value(0,0)[0] << std::endl;
-  std::cout << output.matrix->value(0,0)[1] << std::endl;
-  std::cout << output.matrix->value(0,0)[2] << std::endl;
+  std::cout << output.density.value(0,0) << std::endl;
+  std::cout << output.velocity.value(0,0)[0] << std::endl;
+  std::cout << output.velocity.value(0,0)[1] << std::endl;
   EXPECT_EQ(1,level.width);
 }
 
 TEST(LatticBoltzmannTest, EquilibriumTrue) {
 
-  Level level;
-  LatticeBoltzmannInput input;
-  LatticeBoltzmannOutput output;
-
+  Level level("data/testLevel.txt");
   level.width=1;
   level.height=1;
-  output.matrix = std::make_unique<Array2D<glm::vec3>>(1,1);
-  output.matrix->value(0,0) = glm::vec3(0,0,0);
 
-  LatticeBoltzmann sut( level, false);
+  LatticeBoltzmannInput input;
+  LatticeBoltzmannOutput output(level);
+  output.density.value(0,0) = 0;
+  output.velocity.value(0,0) = glm::vec2(0,0);
+
+  LatticeBoltzmann sut(level);
   sut.compute(input, output);
-  std::cout << "xVelocity" << output.matrix->value(0,0)[0] << std::endl;
-  std::cout << "yVelocity" << output.matrix->value(0,0)[1] << std::endl;
-  std::cout << "Density " << output.matrix->value(0,0)[2] << std::endl;
- 
+  std::cout << "xVelocity=" << output.velocity.value(0,0)[0] << std::endl;
+  std::cout << "yVelocity=" << output.velocity.value(0,0)[1] << std::endl;
+  std::cout << "Density=" << output.density.value(0,0) << std::endl;
+  EXPECT_EQ(1,level.width);
 }

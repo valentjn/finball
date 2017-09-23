190f0e12fd5d642b000fd66a694739b4954b662f
Benedikt Werner <1benediktwerner@gmail.com>
2017-09-23 16:34:50 +0200

    Fixed logging

--- src/Visualization/RendererInput.hpp
+++ src/Visualization/RendererInput.hpp
@@ -38,8 +38,7 @@ public:
 
         for (auto& gameLogicObject : world_objects){
         	if(gameLogicObject.mesh == nullptr){
-				Log::error(
-						"Error: a game_logic object does not have a render mesh, rendering as circle");
+				Log::warn("A game_logic object does not have a render mesh, rendering as circle");
 				gameLogicObject.mesh = &dummyUIMesh;
         	}
         }
@@ -48,9 +47,8 @@ public:
         // first generate necessary meshes, if they are not given.
 		for (const RigidBody& rigidBody : rigidBodyPhysicsOutput.rigid_bodies) {
 			if (m_rigid_body_meshes.count(rigidBody.id) == 0) {
-				Log::info(
-						"WARNING: rigid body with id %d does not have a render mesh, rendering as circle",
-						rigidBody.id);
+				Log::warn("Rigid body with id %d does not have a render mesh, rendering as circle",
+						  rigidBody.id);
 				m_rigid_body_meshes.emplace(rigidBody.id,createCircleMesh(1.f));
 			}
 		}

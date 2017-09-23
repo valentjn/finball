91ee429082149ab10f21a6a0a89939c2115a0918
Benedikt Werner <1benediktwerner@gmail.com>
2017-09-23 23:47:44 +0200

    Benchmarking of components

---
+++ benchmark.py
@@ -0,0 +1,28 @@
+def analyse(file_path):
+    n = 0
+    time = [0]*9
+    percent = [0]*9
+    NAMES = ("Inp", "RB In", "RB", "LB In", "LB", "GL In", "GL", "R In", "R")
+
+    print("Reading", file_path,"...")
+
+    with open(file_path, "r") as f:
+        for line in f:
+            n += 1
+            s = tuple(map(float, line[:-2].split(";")))
+            total = sum(s)
+            for i,e in enumerate(s):
+                time[i] += e
+                if total > 0:
+                    percent[i] += e / total
+
+    print("Finished reading data")
+
+    print("Process\t\tTotal Sec\tPercent")
+    for i in range(9):
+        print("{}:\t\t{:.1f}\t\t{:.1%}".format(NAMES[i], time[i], percent[i]/n))
+
+for i in range(1,8):
+    print("=====", i, "=====")
+    analyse("benchmark{}.txt".format(i))
+    print("\n\n")
--- src/GameController.hpp
+++ src/GameController.hpp
@@ -2,6 +2,8 @@
 #define GAME_CONTROLLER_HPP_
 
 #include <chrono>
+#include <vector>
+#include <fstream>
 
 #include "GameLogic/GameLogic.hpp"
 #include "GameLogic/GameLogicInput.hpp"
@@ -41,26 +43,54 @@ public:
         RendererInput rendererInput;
 
         steady_clock::time_point lastFrame = steady_clock::now();
+        std::vector<steady_clock::time_point> time_points(10);
+        std::remove("benchmark.txt");
+        fstream file;
+        file.open("benchmark.txt", fstream::out | fstream::app);
+        bool benchmark = true;
+        if (!file.is_open()) {
+            Log::error("Failed to open benchmark file!");
+            benchmark = false;
+        }
 
         while (gameLogicOutput.running) {
             // 1. get user input (kinect)
+            time_points.push_back(steady_clock::now());
             userInput.getInput(userInputOutput);
+            time_points.push_back(steady_clock::now());
 
             // 2. do calculations (rigid body, LBM)
             rigidBodyPhysicsInput = RigidBodyPhysicsInput(userInputOutput, latticeBoltzmannOutput);
+            time_points.push_back(steady_clock::now());
             rigidBodyPhysics.compute(rigidBodyPhysicsInput, rigidBodyPhysicsOutput);
+            time_points.push_back(steady_clock::now());
 
             latticeBoltzmannInput = LatticeBoltzmannInput(userInputOutput, rigidBodyPhysicsOutput);
+            time_points.push_back(steady_clock::now());
             latticeBoltzmann.compute(latticeBoltzmannInput, latticeBoltzmannOutput);
+            time_points.push_back(steady_clock::now());
 
             gameLogicInput =
                 GameLogicInput(userInputOutput, rigidBodyPhysicsOutput, latticeBoltzmannOutput);
+            time_points.push_back(steady_clock::now());
             gameLogic.update(gameLogicInput, gameLogicOutput);
+            time_points.push_back(steady_clock::now());
 
             // 3. draw visualization
             rendererInput =
                 RendererInput(gameLogicOutput, rigidBodyPhysicsOutput, latticeBoltzmannOutput);
+            time_points.push_back(steady_clock::now());
             renderer.update(rendererInput);
+            time_points.push_back(steady_clock::now());
+
+            if (benchmark) {
+                for (int i = 0; i < 9; i++) {
+                    duration<float> dur = time_points[i+1] - time_points[i];
+                    file << dur.count() << ";";
+                }
+                file << "\n";
+            }
+            time_points.clear();
 
             if (Log::logLevel >= Log::DEBUG) {
                 steady_clock::time_point thisFrame = steady_clock::now();

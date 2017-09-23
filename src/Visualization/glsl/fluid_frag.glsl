9eb348f4ad81f2ff96a6d9fcdf305352d8c24e39
haslersn <haslersn@fius.informatik.uni-stuttgart.de>
2017-09-23 04:56:28 +0200

    LIC works

--- src/Visualization/glsl/fluid_frag.glsl
+++ src/Visualization/glsl/fluid_frag.glsl
@@ -4,14 +4,13 @@
 uniform sampler2D tex_vecs; // velocities texture
 uniform sampler2D tex_noise; // noise texture
 
-uniform int STEPS=10; // The number of adjacent locations in one direction to use for smearing
+const int STEPS=10; // The number of adjacent locations in one direction to use for smearing
 
 // perfrom line integral convolution
-vec4 lic() {  
+float lic(vec2 normalized_coords) {  
 	const float STEPSIZE = 0.01; // Stepsize to access adjacent texture sample
 
 	//vec2 init_coords = (2.0*coords)-vec2(1.0,1.0); // inital x,y coordinate, shift domain from (0,1) domain to (-1,1)
-	vec2 normalized_coords = gl_FragCoord.xy / textureSize(tex_noise, 0);
 	vec2 v; // the vector field's x and y components
 	vec2 step_coords; // current step x,y coordinate
 	vec4 texture_value = vec4(0.0,0.0,0.0,0.0); // color value at the particular texture coordinate vec4/RGBA
@@ -20,13 +19,9 @@ vec4 lic() {
 	// Start at this fragment's location
 	step_coords = normalized_coords;
 	
-	if(texture(tex_vecs, normalized_coords).y == 0.)
-		return vec4(0.,0.,1.0,1.0);
-
 	// step FORWARD along the vector field
 	for(int i=0; i<STEPS; i++) {
 		v = texture(tex_vecs, normalized_coords).xy;
-		//v = texture2D(tex_vecs, step_coords);
 
 		// use Euler's Method. Get the next approximate point along the curve
 		step_coords += STEPSIZE * v;
@@ -54,24 +49,16 @@ vec4 lic() {
 	// average grayscale value of all samples along the curve, this is the final value for this pixel
 	float average_value = running_total / float(STEPS * 2);
 
-	// use average value for RGB and 1 for A
-
-	//vec4 return_value = vec4(average_value,average_value,average_value,1.0);
-	
-	vec4 return_value = vec4(155.,0.,0.,1.0);
-
-	return return_value;
+	return average_value;
 }
 
 
 layout(location = 0) out vec4 out_color;
 
 void main() {
-	
-
     vec2 normalized_coords = gl_FragCoord.xy / textureSize(tex_noise, 0);
-    vec4 velocity = texture(tex_vecs, normalized_coords).xyxy;
-    out_color = lic();   // run lic; note the return value is of type vec4/RGBA
-    out_color = velocity * 0.5 + 0.5;
+    ivec2 vecs_res = textureSize(tex_vecs, 0);
+    normalized_coords = (normalized_coords * vecs_res + 0.5) / (vecs_res + 1);
+    out_color = vec4(lic(normalized_coords));   // run lic; note the return value is of type vec4/RGBA
 }
 

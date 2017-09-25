387a00523a7f3afd0a1baf48eced9a35d141d100
Marcial Gaißert <st141606@stud.uni-stuttgart.de>
2017-09-25 18:17:54 +0200

    retry initialization when failing

--- src/UserInput/UserInput.cpp
+++ src/UserInput/UserInput.cpp
@@ -20,48 +20,55 @@ using namespace std;
 using namespace std::chrono;
 
 #ifndef WITHOUT_KINECT_LIBRARIES
-void checkErrorCode(const char* msg, XnStatus status) {
-	if(status != XN_STATUS_OK) {
-		Log::error("Kinect initialization failed: %s", msg);
-	}
-}
-#endif
-
-// initialization of user input facilities
-UserInput::UserInput(){
-
-#ifndef WITHOUT_KINECT_LIBRARIES
+bool UserInput::initializeKinect() {
+	if(kinectIsInitialized) return true;
+	
     // init kinect context etc
     context = make_unique<Context>();
     userGenerator = make_unique<UserGenerator>();
     depthGenerator = make_unique<DepthGenerator>();
-    previous_time_point = std::chrono::high_resolution_clock::now();
 	
-	nUsers = 0; nPlayers = MAX_USERS; trackedUsers = 0;
-	for(int k = 0; k < PLAYERS; k++){
-		playerJoined[k] = false;
-	}
-	for(int k = 0; k < MAX_USERS; k++){
-		userIsPlayer[k] = false;
-	}
-
-    // TODO: handle error codes
+	// TODO: handle error codes
     XnStatus errorCode = XN_STATUS_OK;
 
     errorCode = context->Init();
-	checkErrorCode("Can't initialize context", errorCode);
+	if(errorCode != XN_STATUS_OK) {
+		Log::info("Can't initialize context for kinect.");
+		Log::debug("Error code: %i", errorCode);
+		kinectIsInitialized = false;
+		return false;
+	}
+	
 	errorCode = context->SetGlobalMirror(true);
-	checkErrorCode("Can't initialize context", errorCode);
+	if(errorCode != XN_STATUS_OK) {
+		Log::info("Can't initialize context for kinect.");
+		Log::debug("Error code: %i", errorCode);
+		kinectIsInitialized = false;
+		return false;
+	}
+
     errorCode = context->FindExistingNode(XN_NODE_TYPE_DEPTH, *depthGenerator);
-    while(errorCode != XN_STATUS_OK) {
+    if(errorCode != XN_STATUS_OK) {
 		Log::info("Failed to initialize Depth Generator. Retrying...");
         errorCode = depthGenerator->Create(*context);
+		if(errorCode != XN_STATUS_OK) {
+			Log::info("Can't initialize depth generator.");
+			Log::debug("Error code: %i", errorCode);
+			kinectIsInitialized = false;
+			return false;
+		}
     }
 
     errorCode = context->FindExistingNode(XN_NODE_TYPE_USER, *userGenerator);
-    while(errorCode != XN_STATUS_OK) {
+    if(errorCode != XN_STATUS_OK) {
 		Log::info("Failed to initialize User Generator. Retrying...");
         errorCode = userGenerator->Create(*context);
+		if(errorCode != XN_STATUS_OK) {
+			Log::info("Can't initialize user generator.");
+			Log::debug("Error code: %i", errorCode);
+			kinectIsInitialized = false;
+			return false;
+		}
     }
 
     // register callbacks
@@ -78,14 +85,24 @@ UserInput::UserInput(){
             // user lost
         },
         this, hUserCallbacks);
-	checkErrorCode("Can't register user callbacks", errorCode);
+	if(errorCode != XN_STATUS_OK) {
+		Log::info("Can't register user callbacks.");
+		Log::debug("Error code: %i", errorCode);
+		kinectIsInitialized = false;
+		return false;
+	}
 
     errorCode = userGenerator->GetSkeletonCap().RegisterToCalibrationStart(
         [](SkeletonCapability& skeletonCapability, XnUserID nID, void* cookie){
             UserInput* _this = reinterpret_cast<UserInput*>(cookie);
             // calibration start
         }, this, hCalibrationStart);
-	checkErrorCode("Can't register calibration callbacks", errorCode);
+	if(errorCode != XN_STATUS_OK) {
+		Log::info("Can't register calibration start.");
+		Log::debug("Error code: %i", errorCode);
+		kinectIsInitialized = false;
+		return false;
+	}
 
     errorCode = userGenerator->GetSkeletonCap().RegisterToCalibrationComplete(
         [](SkeletonCapability& skeletonCapability, XnUserID nID,
@@ -101,7 +118,13 @@ UserInput::UserInput(){
                     .RequestCalibration(nID, TRUE);
             }
         }, this, hCalibrationComplete);
-	checkErrorCode("Can't register after-calibration callbacks", errorCode);
+	if(errorCode != XN_STATUS_OK) {
+		Log::info("Can't register calibration end.");
+		Log::debug("Error code: %i", errorCode);
+		kinectIsInitialized = false;
+		return false;
+	}
+
 
     // Get Skeleton
     userGenerator->GetSkeletonCap().SetSkeletonProfile(XN_SKEL_PROFILE_ALL);
@@ -109,22 +132,74 @@ UserInput::UserInput(){
     // start generating data
 	Log::debug("Start generating input data...");
     errorCode = context->StartGeneratingAll();
+	if(errorCode != XN_STATUS_OK) {
+		Log::info("Can't generate data.");
+		Log::debug("Error code: %i", errorCode);
+		kinectIsInitialized = false;
+		return false;
+	}
+
+	kinectIsInitialized = true;
+	return true;
+}
 #endif
+
+// initialization of user input facilities
+UserInput::UserInput(){
+
+#ifndef WITHOUT_KINECT_LIBRARIES
+    previous_time_point = std::chrono::high_resolution_clock::now();
+	kinectIsInitialized = false;
+	nUsers = 0; nPlayers = MAX_USERS; trackedUsers = 0;
+	
+	for(int k = 0; k < PLAYERS; k++){
+		playerJoined[k] = false;
+	}
+	for(int k = 0; k < MAX_USERS; k++){
+		userIsPlayer[k] = false;
+	}
+
+	tryInitializingKinect = true;
+	initializeKinect();
+#endif
+
+	for(int k=0; k < PLAYERS; k++){
+		max_angle[k] = 1.5;
+		min_angle[k] = -1.5;
+		zero_angle[k] = 0;
+	}
+	
 }
 
-// finalization of user input facilities
-UserInput::~UserInput() {
-    // TODO
 #ifndef WITHOUT_KINECT_LIBRARIES
-    // cleanup kinect context etc
+void UserInput::finalizeKinect() {	
+	// cleanup kinect context etc
     depthGenerator->Release();
     userGenerator->Release();
     context->Release();
+
+	depthGenerator = nullptr;
+	userGenerator = nullptr;
+	context = nullptr;
+
+	kinectIsInitialized = false;
+}
+#endif
+
+// finalization of user input facilities
+UserInput::~UserInput() {
+#ifndef WITHOUT_KINECT_LIBRARIES
+    finalizeKinect();
 #endif
 }
 
 // process input
 void UserInput::getInput(UserInputOutput &userInputOutput) {
+	// get timing information
+	auto now = high_resolution_clock::now();
+	nanoseconds delta = duration_cast<nanoseconds>(now-previous_time_point);
+
+	// SDL events
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
@@ -135,137 +210,144 @@ void UserInput::getInput(UserInputOutput &userInputOutput) {
     }
 
 #ifndef WITHOUT_KINECT_LIBRARIES
-    // TODO: handle error codes
-    XnStatus errorCode = XN_STATUS_OK;
-
-    context->WaitNoneUpdateAll();
-
-    auto now = high_resolution_clock::now();
-    nanoseconds delta = duration_cast<nanoseconds>(now-previous_time_point);
-
-	XnUInt16 tmpNUsers = MAX_USERS;
-	userGenerator->GetUsers(users, tmpNUsers);
-	nUsers = tmpNUsers;
-
-    for(XnUInt16 i=0; i < nUsers; i++) {
-        if(userGenerator->GetSkeletonCap().IsTracking(users[i])==FALSE) {
-            continue;
-        }
-
-        XnSkeletonJointTransformation leftHandJoint, rightHandJoint, headJoint;
-		userGenerator->GetSkeletonCap()
-			.GetSkeletonJoint(users[i],XN_SKEL_LEFT_HAND, leftHandJoint);
-		userGenerator->GetSkeletonCap()
-			.GetSkeletonJoint(users[i],XN_SKEL_RIGHT_HAND, rightHandJoint);
-		userGenerator->GetSkeletonCap()
-			.GetSkeletonJoint(users[i],XN_SKEL_HEAD, headJoint);
-
-		if(leftHandJoint.position.position.Y - threshold
-			> headJoint.position.position.Y
-			&& rightHandJoint.position.position.Y - threshold
-			> headJoint.position.position.Y) {
-
-			for(XnUInt16 j=0; j < PLAYERS; j++) {
-				if(!playerJoined[j] && !userIsPlayer[i]) {
-					players[j] = users[i];
-					playerJoined[j] = true;
-					userIsPlayer[i] = true;
-					playerIndices[j] = i;
-					if(nPlayers==trackedUsers)
-						nPlayers++;
-					trackedUsers++;
-				}
-			}
+	if (!kinectIsInitialized) {
+		if(tryInitializingKinect) {
+			// try again to initialize the kinect
+			finalizeKinect();
+			initializeKinect();
 		}
-    }
+	} else {
+		// TODO: handle error codes
+		XnStatus errorCode = XN_STATUS_OK;
 
-	for(XnUInt16 k = 0; k < nPlayers; k++) {
-		if(playerJoined[k]) {
-			if(!userGenerator->GetSkeletonCap().IsTracking(players[k])) {
-				userInputOutput.playerIsTracked[k] = false;
-				playerJoined[k] = false;
-				userIsPlayer[playerIndices[k]] = false;
-				trackedUsers--;
+		context->WaitNoneUpdateAll();
+
+		XnUInt16 tmpNUsers = MAX_USERS;
+		userGenerator->GetUsers(users, tmpNUsers);
+		nUsers = tmpNUsers;
+
+		for(XnUInt16 i=0; i < nUsers; i++) {
+			if(userGenerator->GetSkeletonCap().IsTracking(users[i])==FALSE) {
 				continue;
 			}
 
-			userInputOutput.playerIsTracked[k] = true;
+			XnSkeletonJointTransformation leftHandJoint, rightHandJoint, headJoint;
+			userGenerator->GetSkeletonCap()
+				.GetSkeletonJoint(users[i],XN_SKEL_LEFT_HAND, leftHandJoint);
+			userGenerator->GetSkeletonCap()
+				.GetSkeletonJoint(users[i],XN_SKEL_RIGHT_HAND, rightHandJoint);
+			userGenerator->GetSkeletonCap()
+				.GetSkeletonJoint(users[i],XN_SKEL_HEAD, headJoint);
 
-			XnSkeletonJointTransformation leftHandJoint, rightHandJoint,
-				leftElbowJoint, rightElbowJoint;
-			userGenerator->GetSkeletonCap().GetSkeletonJoint(players[k],
-				XN_SKEL_LEFT_HAND, leftHandJoint);
-			userGenerator->GetSkeletonCap().GetSkeletonJoint(players[k],
-				XN_SKEL_RIGHT_HAND, rightHandJoint);
-			userGenerator->GetSkeletonCap().GetSkeletonJoint(players[k],
-				XN_SKEL_LEFT_ELBOW, leftElbowJoint);
-			userGenerator->GetSkeletonCap().GetSkeletonJoint(players[k],
-				XN_SKEL_RIGHT_ELBOW, rightElbowJoint);
+			if(leftHandJoint.position.position.Y - threshold
+				> headJoint.position.position.Y
+				&& rightHandJoint.position.position.Y - threshold
+				> headJoint.position.position.Y) {
 
-			double ldx = leftHandJoint.position.position.X
-				- leftElbowJoint.position.position.X;
-			double ldy = leftHandJoint.position.position.Y
-				- leftElbowJoint.position.position.Y;
-			double rdx = rightHandJoint.position.position.X
-				- rightElbowJoint.position.position.X;
-			double rdy = rightHandJoint.position.position.Y
-				- rightElbowJoint.position.position.Y;
-
-			userInputOutput.leftAngle[k] = std::atan2(ldy, ldx) - zero_angle[k];
-			userInputOutput.rightAngle[k] = std::atan2(rdy, rdx) + zero_angle[k];
-
-			if(userInputOutput.leftAngle[k] < -M_PI) {
-				userInputOutput.leftAngle[k] += 2*M_PI;
+				for(XnUInt16 j=0; j < PLAYERS; j++) {
+					if(!playerJoined[j] && !userIsPlayer[i]) {
+						players[j] = users[i];
+						playerJoined[j] = true;
+						userIsPlayer[i] = true;
+						playerIndices[j] = i;
+						if(nPlayers==trackedUsers)
+							nPlayers++;
+						trackedUsers++;
+					}
+				}
 			}
-			if(userInputOutput.rightAngle[k] > M_PI) {
-				userInputOutput.rightAngle[k] -= 2*M_PI;
-			}
-			double rightPsi = copysign(M_PI, userInputOutput.rightAngle[k])
-				- userInputOutput.rightAngle[k];
-
-			if(userInputOutput.leftAngle[k] > max_angle[k]) {
-				userInputOutput.leftAngle[k] = max_angle[k]; 
-			} else if(userInputOutput.leftAngle[k] < min_angle[k]) {
-				userInputOutput.leftAngle[k] = min_angle[k];
-			}
-
-			if(rightPsi > max_angle[k]) {
-				userInputOutput.rightAngle[k] = M_PI - max_angle[k]; 
-			} else if(rightPsi < min_angle[k]) {
-				userInputOutput.rightAngle[k] = -M_PI -min_angle[k];
-			}
-
-			rightPsi = copysign(M_PI, userInputOutput.rightAngle[k])
-				- userInputOutput.rightAngle[k];
-
-			double previousRightPsi = copysign(M_PI, previousRightAngles[k])
-				- previousRightAngles[k];
-			double ldAngle = userInputOutput.leftAngle[k] - previousLeftAngles[k];
-			double rdAngle = rightPsi - previousRightPsi;
-			userInputOutput.leftVelocity[k] = (1000000*ldAngle) / delta.count();
-			userInputOutput.rightVelocity[k] = (1000000*rdAngle) / delta.count();
-
-			
-			if(ldAngle < MIN_DIFFERENCE && ldAngle > -MIN_DIFFERENCE) {
-				userInputOutput.leftAngle[k] = previousLeftAngles[k];
-				userInputOutput.leftVelocity[k] = 0;
-			}
-			if(rdAngle < MIN_DIFFERENCE && rdAngle > -MIN_DIFFERENCE) {
-				userInputOutput.rightAngle[k] = previousRightAngles[k];
-				userInputOutput.rightVelocity[k] = 0;
-			}
-			
 		}
+
+		for(XnUInt16 k = 0; k < nPlayers; k++) {
+			if(playerJoined[k]) {
+				if(!userGenerator->GetSkeletonCap().IsTracking(players[k])) {
+					userInputOutput.playerIsTracked[k] = false;
+					playerJoined[k] = false;
+					userIsPlayer[playerIndices[k]] = false;
+					trackedUsers--;
+					continue;
+				}
+
+				userInputOutput.playerIsTracked[k] = true;
+
+				XnSkeletonJointTransformation leftHandJoint, rightHandJoint,
+					leftElbowJoint, rightElbowJoint;
+				userGenerator->GetSkeletonCap().GetSkeletonJoint(players[k],
+					XN_SKEL_LEFT_HAND, leftHandJoint);
+				userGenerator->GetSkeletonCap().GetSkeletonJoint(players[k],
+					XN_SKEL_RIGHT_HAND, rightHandJoint);
+				userGenerator->GetSkeletonCap().GetSkeletonJoint(players[k],
+					XN_SKEL_LEFT_ELBOW, leftElbowJoint);
+				userGenerator->GetSkeletonCap().GetSkeletonJoint(players[k],
+					XN_SKEL_RIGHT_ELBOW, rightElbowJoint);
+
+				double ldx = leftHandJoint.position.position.X
+					- leftElbowJoint.position.position.X;
+				double ldy = leftHandJoint.position.position.Y
+					- leftElbowJoint.position.position.Y;
+				double rdx = rightHandJoint.position.position.X
+					- rightElbowJoint.position.position.X;
+				double rdy = rightHandJoint.position.position.Y
+					- rightElbowJoint.position.position.Y;
+
+				userInputOutput.leftAngle[k] = std::atan2(ldy, ldx) - zero_angle[k];
+				userInputOutput.rightAngle[k] = std::atan2(rdy, rdx) + zero_angle[k];
+
+				if(userInputOutput.leftAngle[k] < -M_PI) {
+					userInputOutput.leftAngle[k] += 2*M_PI;
+				}
+				if(userInputOutput.rightAngle[k] > M_PI) {
+					userInputOutput.rightAngle[k] -= 2*M_PI;
+				}
+				double rightPsi = copysign(M_PI, userInputOutput.rightAngle[k])
+					- userInputOutput.rightAngle[k];
+
+				if(userInputOutput.leftAngle[k] > max_angle[k]) {
+					userInputOutput.leftAngle[k] = max_angle[k]; 
+				} else if(userInputOutput.leftAngle[k] < min_angle[k]) {
+					userInputOutput.leftAngle[k] = min_angle[k];
+				}
+
+				if(rightPsi > max_angle[k]) {
+					userInputOutput.rightAngle[k] = M_PI - max_angle[k]; 
+				} else if(rightPsi < min_angle[k]) {
+					userInputOutput.rightAngle[k] = -M_PI -min_angle[k];
+				}
+
+				rightPsi = copysign(M_PI, userInputOutput.rightAngle[k])
+					- userInputOutput.rightAngle[k];
+
+				double previousRightPsi = copysign(M_PI, previousRightAngles[k])
+					- previousRightAngles[k];
+				double ldAngle = userInputOutput.leftAngle[k] - previousLeftAngles[k];
+				double rdAngle = rightPsi - previousRightPsi;
+				userInputOutput.leftVelocity[k] = (1000000*ldAngle) / delta.count();
+				userInputOutput.rightVelocity[k] = (1000000*rdAngle) / delta.count();
+
+			
+				if(ldAngle < MIN_DIFFERENCE && ldAngle > -MIN_DIFFERENCE) {
+					userInputOutput.leftAngle[k] = previousLeftAngles[k];
+					userInputOutput.leftVelocity[k] = 0;
+				}
+				if(rdAngle < MIN_DIFFERENCE && rdAngle > -MIN_DIFFERENCE) {
+					userInputOutput.rightAngle[k] = previousRightAngles[k];
+					userInputOutput.rightVelocity[k] = 0;
+				}
+			
+			}
+		}
+
+		for(int k = 0; k < PLAYERS; k++){
+			previousLeftAngles[k] = userInputOutput.leftAngle[k];
+			previousRightAngles[k] = userInputOutput.rightAngle[k];
+		}
+		userInputOutput.start = std::all_of(playerJoined, playerJoined+PLAYERS,
+			[](bool x){return x;});
+
 	}
 
-	previous_time_point = now;
-	for(int k = 0; k < PLAYERS; k++){
-		previousLeftAngles[k] = userInputOutput.leftAngle[k];
-		previousRightAngles[k] = userInputOutput.rightAngle[k];
-	}
-
-	userInputOutput.start = std::all_of(playerJoined, playerJoined+PLAYERS,
-		[](bool x){return x;});
-	
 #endif
+	
+	previous_time_point = now;
+		
 }
--- src/UserInput/UserInput.hpp
+++ src/UserInput/UserInput.hpp
@@ -26,15 +26,17 @@ typedef XnUserID user_id_t;
 
 class UserInput {
 private:
+	static const int MAX_USERS = 4;
+	static const int PLAYERS = 2;
+	static constexpr const double MIN_DIFFERENCE = 0.1;
+	
 #ifndef WITHOUT_KINECT_LIBRARIES
     std::unique_ptr<xn::Context> context;
     std::unique_ptr<xn::UserGenerator> userGenerator;
     std::unique_ptr<xn::DepthGenerator> depthGenerator;
-#endif
 
-	static const int MAX_USERS = 4;
-	static const int PLAYERS = 2;
-	static constexpr const double MIN_DIFFERENCE = 0.1;
+	bool kinectIsInitialized = false;
+	bool tryInitializingKinect = true;
 	
 	user_id_t players[PLAYERS];
 	user_id_t users[MAX_USERS];
@@ -44,13 +46,17 @@ private:
 	int nPlayers, nUsers, trackedUsers;
 	float threshold = 0;
 
+	bool initializeKinect();
+	void finalizeKinect();
+#endif
+
 	std::chrono::high_resolution_clock::time_point previous_time_point;
 	float previousLeftAngles[PLAYERS];
 	float previousRightAngles[PLAYERS];
 
-	float max_angle[2] = { 1.5, 1.5 };
-	float min_angle[2] = { -1.5, -1.5 };
-	float zero_angle[2] = { 0, 0 };
+	float max_angle[PLAYERS];
+	float min_angle[PLAYERS];
+	float zero_angle[PLAYERS];
 
 public:
 	UserInput();

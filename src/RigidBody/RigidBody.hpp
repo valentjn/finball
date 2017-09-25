11356d6d036d57a11876e6dc546a661ce7689f20
Benedikt Werner <1benediktwerner@gmail.com>
2017-09-25 15:35:33 +0200

    New level-RigidBody data structures

--- src/RigidBody/RigidBody.hpp
+++ src/RigidBody/RigidBody.hpp
@@ -3,17 +3,41 @@
 
 #include <glm/vec2.hpp>
 
+using namespace glm;
+
 struct RigidBody {
     int id;
-    glm::vec2 position;
-    bool isFixed;
-    float angle;
+    vec2 position;
+    int mass;
+    float rotation;
+
+    RigidBody(int id, float x, float y, int mass=1, float rotation=0.f) : id(id), position(x, y), mass(mass), rotation(rotation) {}
+    virtual ~RigidBody() {}
+};
+
+struct RigidBodyRect : RigidBody {
+    float width;
+    float height;
+
+    RigidBodyRect(int id, float x, float y, int width, int height, int mass=1, float rotation=0.f) : RigidBody(id, x, y, mass, rotation), width(width), height(height) {}
+};
+
+struct RigidBodyCircle : RigidBody {
     float radius;
 
-    RigidBody(int id, float x, float y, bool isFixed) : id(id), position(x, y), isFixed(isFixed),
-        angle(0), radius(1.f) {}
-    RigidBody(int id, float x, float y) : RigidBody(id, x, y, true) {}
-    virtual ~RigidBody() {}
+    RigidBodyCircle(int id, float x, float y, float radius=1.f, int mass=1, float rotation=0.f) : RigidBody(id, x, y, mass, rotation), radius(radius) {}
+};
+
+struct RigidBodyTriangle : RigidBody {
+    vec2 points[3];
+
+    RigidBodyTriangle(int id, float x, float y, vec2 p1, vec2 p2, vec2 p3, int mass=1, float rotation=0.f) : RigidBody(id, x, y, mass, rotation) {
+        points[0] = p1;
+        points[1] = p2;
+        points[2] = p3;
+    }
+
+    RigidBodyTriangle(int id, float x, float y, vec2 p2, vec2 p3, int mass=1, float rotation=0.f) : RigidBodyTriangle(id, x, y, vec2(0, 0), p2, p3, mass, rotation) {}
 };
 
 #endif

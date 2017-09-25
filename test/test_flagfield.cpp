b6b59ae39d41d4ec8c19f0c60246f6a574e1596f
Frank von Bleifrei <freifrauvonbleifrei@gmx.net>
2017-09-25 07:15:20 -0400

    started on fall-stop test

--- test/test_flagfield.cpp
+++ test/test_flagfield.cpp
@@ -40,9 +40,6 @@ TEST(RigidBodyTest, static64) {
       sut.compute(input, output);
   }
 
-  //std::cout << output.density.value(0,0) << std::endl;
-  //std::cout << output.velocity.value(0,0)[0] << std::endl;
-  //std::cout << output.velocity.value(0,0)[1] << std::endl;
   bool changed = true;
   for (int i = 0; i<level.width ; ++i){
 
@@ -96,10 +93,6 @@ TEST(RigidBodyTest, gravity64) {
   for (int i = 0; i < level.width/2 - 5 ; ++i){
       sut.compute(input, output);
   }
-
-  //std::cout << output.density.value(0,0) << std::endl;
-  //std::cout << output.velocity.value(0,0)[0] << std::endl;
-  //std::cout << output.velocity.value(0,0)[1] << std::endl;
   bool changed = false;
   for (int i = 0; i<level.width ; ++i){
 
@@ -115,4 +108,58 @@ TEST(RigidBodyTest, gravity64) {
   if(! changed){
     printFlagField(init_flagfield);
   }
+}
+
+
+// if the rigid body is above the horizontal slope in the beginning,
+//AND we have gravity,
+// then the x coordinate should not change in the first few time steps
+TEST(RigidBodyTest, stop64) {
+
+  Level level("data/testLevel.txt");
+  level.rigidBodies = vector<RigidBody>();
+  level.rigidBodies.push_back(RigidBody(666, level.width/4, level.height*0.9, false));
+
+  RigidBodyPhysicsOutput output(level);
+  RigidBodyPhysicsInput input;
+
+  UserInputOutput userInputOutput;
+  LatticeBoltzmannOutput latticeBoltzmannOutput(level);
+  input = RigidBodyPhysicsInput(userInputOutput, latticeBoltzmannOutput);
+
+  for (int i = 0; i<level.width ; ++i){
+
+      for (int j = 0; j<level.height ; ++j){
+          latticeBoltzmannOutput.density.value(i,j) = 1;
+          latticeBoltzmannOutput.velocity.value(i,j) = glm::vec2(0,0);
+      }
+  }
+
+
+  RigidBodyPhysics sut(level);
+  sut.setGravity(true);
+// for some reason, the first field is zero-initialized
+  sut.compute(input, output);
+
+  Array2D<Level::CellType> init_flagfield = output.grid_objects;
+
+  for (int i = 0; i < level.width ; ++i){
+      sut.compute(input, output);
+  }
+
+  bool changed = false;
+  for (int i = 0; i<level.width ; ++i){
+
+      for (int j = 0; j<level.height ; ++j){
+        if(init_flagfield.value(i,j) != output.grid_objects.value(i,j)){
+            changed = true;
+        }
+      }
+  }
+
+  EXPECT_TRUE(changed);
+  //if(! changed){
+  printFlagField(init_flagfield);
+  printFlagField(output.grid_objects);
+  //}
 }

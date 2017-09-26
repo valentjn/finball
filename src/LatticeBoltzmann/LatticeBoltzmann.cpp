952c01d32dce02bae2f835735c19de792e07df04
Katharina Tkotz <katharina.tkotz@online.de>
2017-09-26 11:18:25 +0200

    remove output

--- src/LatticeBoltzmann/LatticeBoltzmann.cpp
+++ src/LatticeBoltzmann/LatticeBoltzmann.cpp
@@ -95,9 +95,6 @@ void LatticeBoltzmann::handleBoundaries(const LatticeBoltzmannInput &input)
                                                          fi_New.value(x, y)[z] - 2 / (c*c) *
                                                       density*w[z]/(4.*iter)*(input.velocities.value(x,y)[0]*cx[z] +
                                                     input.velocities.value(x,y)[1]*cy[z]);
-                                            if (input.velocities.value(x,y)[0] != 0 || input.velocities.value(x,y)[1] !=0){
-                                                std::cout << input.velocities.value(x,y)[0] << " " << input.velocities.value(x,y)[1] <<  std::endl;
-                                            }
                                             fi_New.value(x, y)[z] = 0.0;
                                             }
 					}

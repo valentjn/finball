618a9bca3335472b465ae880275176b1f2c8c754
Sebastian Hasler <sebastian.hasler@gmx.net>
2017-09-28 01:32:32 +0200

    Fix transparency support

--- src/Visualization/glsl/world_frag.glsl
+++ src/Visualization/glsl/world_frag.glsl
@@ -11,12 +11,12 @@ layout(location = 0) out vec4 out_color;
 
 void main() {
     if (mode == 0)
-	    out_color = vec4(frag_color, 1);
+	      out_color = vec4(frag_color, 1);
     else {
         out_color = texture(tex, frag_color.xy);
-		if (out_color.a == 0)
-			discard;
-	}
+		    if (out_color.a <= 0.5)
+			      discard;
+	  }
 
     gl_FragDepth = frag_depth;
 }

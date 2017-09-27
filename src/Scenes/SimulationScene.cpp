0bcc032798b6373246f859d49411d142c678e41d
Sebastian Hasler <sebastian.hasler@gmx.net>
2017-09-27 23:38:41 +0200

    Run game logic with simulation rate

--- src/Scenes/SimulationScene.cpp
+++ src/Scenes/SimulationScene.cpp
@@ -59,81 +59,8 @@ float SimulationScene::simulation()
 	rigidBodyPhysics.run(context.parameters->simulationRate, running);
 	latticeBoltzmann.run(context.parameters->simulationRate, running);
 	userInput.run(context.parameters->simulationRate, running);
-	gameLogic.run(context.parameters->frameRate, running);
+	gameLogic.run(context.parameters->simulationRate, running);
 	renderer.run(context.parameters->frameRate, running, true);
-
-    /*steady_clock::time_point lastFrame = steady_clock::now();
-    bool running = true;
-
-    Timer timer([&]() {
-        // 1. get user input (kinect)
-        userInput.getInput(userInputOutput.writeBuffer());
-        userInputOutput.swap();
-
-        userInputOutput.lock();
-        rigidBodyPhysicsOutput.lock();
-        latticeBoltzmannOutput.lock();
-        GameLogicInput gameLogicInput(userInputOutput.readBuffer(),
-                                      rigidBodyPhysicsOutput.readBuffer(),
-                                      latticeBoltzmannOutput.readBuffer());
-        latticeBoltzmannOutput.unlock();
-        rigidBodyPhysicsOutput.unlock();
-        userInputOutput.unlock();
-
-        gameLogic.update(gameLogicInput, gameLogicOutput.writeBuffer());
-        running = gameLogicOutput.writeBuffer().running;
-        gameLogicOutput.swap();
-
-        // 3. draw visualization
-        gameLogicOutput.lock();
-        rigidBodyPhysicsOutput.lock();
-        latticeBoltzmannOutput.lock();
-        rendererInput.process(
-			gameLogicOutput.readBuffer(),
-            rigidBodyPhysicsOutput.readBuffer(),
-            latticeBoltzmannOutput.readBuffer());
-        latticeBoltzmannOutput.unlock();
-        rigidBodyPhysicsOutput.unlock();
-        gameLogicOutput.unlock();
-
-        renderer.update(rendererInput);
-
-        if (Log::logLevel >= Log::DEBUG) {
-            steady_clock::time_point thisFrame = steady_clock::now();
-            duration<float> duration = thisFrame - lastFrame;
-            Log::debug("FPS: %f", 1 / duration.count());
-            lastFrame = thisFrame;
-        }
-    });
-
-    Timer simulationTimer([&]() {
-        // 2. do calculations (rigid body, LBM)
-        userInputOutput.lock();
-        latticeBoltzmannOutput.lock();
-        RigidBodyPhysicsInput rigidBodyPhysicsInput(userInputOutput.readBuffer(), latticeBoltzmannOutput.readBuffer());
-        latticeBoltzmannOutput.unlock();
-        userInputOutput.unlock();
-
-        rigidBodyPhysics.compute(rigidBodyPhysicsInput, rigidBodyPhysicsOutput.writeBuffer());
-        rigidBodyPhysicsOutput.swap();
-
-        rigidBodyPhysicsOutput.lock();
-        LatticeBoltzmannInput latticeBoltzmannInput(rigidBodyPhysicsOutput.readBuffer());
-        rigidBodyPhysicsOutput.unlock();
-
-        latticeBoltzmann.compute(latticeBoltzmannInput, latticeBoltzmannOutput.writeBuffer());
-        latticeBoltzmannOutput.swap();
-    });
-
-    context.music->play("data/GameTheme.mp3");
-
-    std::thread simulationThread([&]() {
-        simulationTimer.start(1000 / context.parameters->simulationRate, running);
-    });
-
-    timer.start(1000 / context.parameters->frameRate, running);
-
-    simulationThread.join();*/
 
     return gameLogic.getOutput().score;
 }
